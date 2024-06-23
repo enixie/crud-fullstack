6b8bed6921d8f981ef7e31d10937cb4c985b37a5
Evelyn Vasconcelos <enixiegamer@gmail.com>
2024-06-23 19:15:27 -0300

    Adicionado toast para erros e função handleDelete para exclusão de usuários

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import GlobalStyle from "./styles/global";
 import styled from "styled-components";
 import Form from "./Form.js";
-import { ToastContainer } from "react-toastify";
+import Grid from "./components/Grid.js";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 const Container = styled.div`
   width: 100%;
@@ -14,16 +17,41 @@ const Container = styled.div`
   gap: 10px;
 `;
 
-const Title = styled.h2`
-  /* Estilos adicionais para o título, se necessário */
-`;
+const Title = styled.h2``;
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState([null]);
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [setUsers]);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:8800/${id}`);
+      toast.success("Usuário deletado com sucesso");
+      getUsers();
+    } catch (error) {
+      toast.error("Erro ao deletar o usuário");
+    }
+  };
+  
   return (
     <>
       <Container>
         <Title>USUÁRIOS</Title>
-        <Form /> {/* Renderiza o componente Form */}
+        <Form />
+        <Grid users={users} handleDelete={handleDelete} />
       </Container>
       <ToastContainer autoClose={3000} position="bottom-left" />
       <GlobalStyle />
